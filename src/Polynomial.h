1d1d5df4683a75e2b2d0b3308509873806697969
DrValani <yogenv@yahoo.com>
2012-08-28 07:40:18 +0100

    cleaned up polynomial.cc and polynomial.h removed unwanted functions

--- src/Polynomial.h
+++ src/Polynomial.h
@@ -27,10 +27,7 @@ public:
 	Polynomial operator-(const Polynomial &p);
 	Polynomial operator*(const Polynomial &p);  //Mulitply two polynomials
 	Polynomial operator*(const int &x) const;  // Mutiply polynomial with integer
-	Polynomial operator^(const int &x) const;
-	bool operator==(const Polynomial &p) const;
 	void operator+=(const Polynomial &p);
-	bool equals(const Polynomial &p) const;
 	void clear();
 	void push_back(const int coeff, const int expo);
 	template<class T>
@@ -43,19 +40,15 @@ public:
 	int getDegree() const {
 		return degree;
 	}
-	void setDegree(int x) {
-		degree = x;
-	}
 	void plusPower(const Polynomial &p, const int pow);
 	bool isZero() {
 		return ((degree == 0) && (summands[0] == 0));
 	}
 	void multiPlus(const Polynomial &p, const int &ct);
 private:
+	const bool isInBounds(const int &expo){return expo < MAXPOLYSIZE;}
 	mpz_class summands[MAXPOLYSIZE];
 	int degree;
 };
-
-
 
 #endif /* POLYNOMIAL_H_ */
